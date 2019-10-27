578bfb5b2c019f869cc276ff1519ecac641fb6bc
Charles Ging <charlesging@Charless-iMac.lan1>
2019-10-27 14:22:04 -0500

    remove individual code submit handlers. delegated to document

--- express_playground/js/app.js
+++ express_playground/js/app.js
@@ -1,15 +1,18 @@
 document.addEventListener("DOMContentLoaded", () => {
   const codemirrors = setAllCodemirrorObjects();
-  const btnCC1 = document.getElementById("btn-codecell-1");
-  const btnCC2 = document.getElementById("btn-codecell-2");
+
+  // const btnCC1 = document.getElementById("btn-codecell-1");
+  // const btnCC2 = document.getElementById("btn-codecell-2");
   const btnMD1 = document.getElementById("render-md-1");
   const mdC1 = document.getElementById("md-cell-1");
 
-  btnCC1.addEventListener("click", () => {
-    const cellNumber = 1;
-    const codeToRender = allCodeUpToCell(cellNumber, codemirrors);
-    const codeResult = document.getElementById(`codecell-${cellNumber}-result`);
+  const handleCodeSubmit = event => {
+    let cellNum = +event.target.dataset.button;
+    // debugger;
+    codeToRender = allCodeUpToCell(cellNum, codemirrors);
+    codeResult = document.getElementById(`codecell-${cellNum}-result`);
     const json = JSON.stringify({ userCode: codeToRender });
+
     const request = new XMLHttpRequest();
 
     request.open("POST", "/");
@@ -23,28 +26,73 @@ document.addEventListener("DOMContentLoaded", () => {
 
       codeResult.textContent = parseRubyOutput(resultString);
     });
+  };
+
+  document.addEventListener("click", event => {
+    if (event.target.className === "code-submit") {
+      handleCodeSubmit(event);
+      // let cellNum = +event.target.dataset.button;
+      // // debugger;
+      // codeToRender = allCodeUpToCell(cellNum, codemirrors);
+      // codeResult = document.getElementById(`codecell-${cellNum}-result`);
+      // const json = JSON.stringify({ userCode: codeToRender });
+
+      // const request = new XMLHttpRequest();
+
+      // request.open("POST", "/");
+      // request.setRequestHeader("Content-Type", "application/json");
+      // request.responseType = "json";
+
+      // request.send(json);
+
+      // request.addEventListener("load", () => {
+      //   const resultString = request.response.result;
+
+      //   codeResult.textContent = parseRubyOutput(resultString);
+      // });
+    }
   });
 
-  // want to replace this hard-coded anti-DRY with named cb func for event listener
-  btnCC2.addEventListener("click", () => {
-    const cellNumber = 2;
-    const codeToRender = allCodeUpToCell(cellNumber, codemirrors);
-    const codeResult = document.getElementById(`codecell-${cellNumber}-result`);
-    const json = JSON.stringify({ userCode: codeToRender });
-    const request = new XMLHttpRequest();
+  // btnCC1.addEventListener("click", () => {
+  //   const cellNumber = 1;
+  //   const codeToRender = allCodeUpToCell(cellNumber, codemirrors);
+  //   const codeResult = document.getElementById(`codecell-${cellNumber}-result`);
+  //   const json = JSON.stringify({ userCode: codeToRender });
+  //   const request = new XMLHttpRequest();
 
-    request.open("POST", "/");
-    request.setRequestHeader("Content-Type", "application/json");
-    request.responseType = "json";
+  //   request.open("POST", "/");
+  //   request.setRequestHeader("Content-Type", "application/json");
+  //   request.responseType = "json";
 
-    request.send(json);
+  //   request.send(json);
 
-    request.addEventListener("load", () => {
-      const resultString = request.response.result;
+  //   request.addEventListener("load", () => {
+  //     const resultString = request.response.result;
 
-      codeResult.textContent = parseRubyOutput(resultString);
-    });
-  });
+  //     codeResult.textContent = parseRubyOutput(resultString);
+  //   });
+  // });
+
+  // // want to replace this hard-coded anti-DRY with named cb func for event listener
+  // btnCC2.addEventListener("click", () => {
+  //   const cellNumber = 2;
+  //   const codeToRender = allCodeUpToCell(cellNumber, codemirrors);
+  //   const codeResult = document.getElementById(`codecell-${cellNumber}-result`);
+  //   const json = JSON.stringify({ userCode: codeToRender });
+  //   const request = new XMLHttpRequest();
+
+  //   request.open("POST", "/");
+  //   request.setRequestHeader("Content-Type", "application/json");
+  //   request.responseType = "json";
+
+  //   request.send(json);
+
+  //   request.addEventListener("load", () => {
+  //     const resultString = request.response.result;
+
+  //     codeResult.textContent = parseRubyOutput(resultString);
+  //   });
+  // });
 
   // extract and render markdown
   const mdCode = CodeMirror.fromTextArea(mdC1, {
