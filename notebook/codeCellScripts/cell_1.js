323eada36e9c4904709994378a075b2dc41c10bb
William Mills <millswilliamrobert@gmail.com>
2019-11-06 10:52:37 -0500

    added Python functionality

--- notebook/codeCellScripts/cell_1.js
+++ notebook/codeCellScripts/cell_1.js
@@ -1,9 +1,6 @@
 console.log("boo!");
 const a = [1, 2, 3, 4, 5];
 a;
-let i = 0;
-
-while (i < 10) {
-   console.log("HI")
-    i++;
-}
+console.log("In code cell 2");
+const redpoint = [5, 5, 5];
+redpoint.map(level => level += 4);
