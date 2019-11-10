e434dbf630d706fde86c7917130699bc126a07ea
ridergit <benjaminriderharvey@gmail.com>
2019-11-10 16:48:00 -0500

    Reorganizes file structure (remove notebook folder), adds .gitignore to ignore node_modules and codeCellScripts

---
+++ libs/modules/repl.js
@@ -0,0 +1,85 @@
+const pty = require("node-pty");
+const stripAnsi = require("strip-ansi");
+
+const repl = {
+  execute: (codeString, lang) => {
+    console.log("BEFORE REPL EXECUTE");
+
+    let replExitMessage;
+    let replType;
+
+    switch (lang) {
+      case "RUBY":
+        replExitMessage = "exit\r";
+        replType = "irb";
+        break;
+      case "JAVASCRIPT":
+        replExitMessage = ".exit\r";
+        replType = "node";
+        break;
+      case "PYTHON":
+        replExitMessage = "exit()\r";
+        replType = "python";
+        break;
+    }
+
+    return new Promise(resolve => {
+      const node = pty.spawn(replType);
+      let returnData = "";
+      node.onData(data => (returnData += stripAnsi(data)));
+      node.write(codeString + replExitMessage);
+      node.on("exit", () => {
+        // assumes REPL is finished, and data is captured, and written to returnData
+        console.log("AFTER REPL EXECUTE");
+        resolve(returnData);
+      });
+    });
+  },
+  parseOutput: (returnData, lang) => {
+    switch (lang) {
+      case "RUBY":
+        return parseRubyOutput(returnData);
+      case "JAVASCRIPT":
+        return parseJSOutput(returnData);
+      case "PYTHON":
+        return parsePythonOutput(returnData);
+    }
+  }
+};
+
+const parseRubyOutput = returnData => {
+  return new Promise(resolve => {
+    const byOutput = returnData.split("=>");
+    const dirtyReturnValue = byOutput[byOutput.length - 1];
+    const indexCleanStops = dirtyReturnValue.indexOf("2.4.1"); // fix hard-coding?
+    const cleanReturnValue = dirtyReturnValue.slice(0, indexCleanStops);
+    resolve(cleanReturnValue);
+  });
+};
+
+const parseJSOutput = returnData => {
+  return new Promise(resolve => {
+    const byOutput = returnData.split(">");
+    const dirtyReturnValue = byOutput[byOutput.length - 2];
+    const cleanReturnValue = extractCleanJSReturnValue(dirtyReturnValue);
+    resolve(cleanReturnValue);
+  });
+};
+
+const parsePythonOutput = returnData => {
+  return new Promise(resolve => {
+    const byOutput = returnData.split(">>>");
+    const dirtyReturnValue = byOutput[byOutput.length - 2];
+    const indexCleanStarts = dirtyReturnValue.indexOf("\n");
+    const cleanReturnValue = dirtyReturnValue.slice(indexCleanStarts);
+    resolve(cleanReturnValue);
+  });
+};
+
+const extractCleanJSReturnValue = string => {
+  const newlines = [...string.matchAll(/\n/g)];
+
+  return string.slice(newlines[newlines.length - 2].index + 1);
+};
+
+module.exports = repl;
---
+++ public/js/app.js
@@ -0,0 +1,236 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const codemirrors = setAllCodemirrorObjects();
+  const btnMD1 = document.getElementById("render-md-1");
+  const mdC1 = document.getElementById("md-cell-1");
+  const ws = new WebSocket("ws://localhost:3000");
+
+  ws.onopen = event => {
+    // receiving the message from server
+    let currentCell = 0;
+    ws.onmessage = message => {
+      message = JSON.parse(message.data);
+
+      switch (message.type) {
+        case "stdout":
+          // slice off empty string when split on newline
+          const stdoutArr = message.data.split("\n").slice(0, -1);
+          stdoutArr.forEach(message => {
+            if (message === "DELIMITER") {
+              currentCell += 1;
+            } else {
+              const outputUl = document.getElementById(
+                `codecell-${currentCell}-output`
+              );
+              appendLi(outputUl, message);
+            }
+          });
+          break;
+        case "stderr":
+          const outputUl = document.getElementById(
+            `codecell-${currentCell}-error`
+          );
+
+          appendLi(outputUl, message.data);
+          break;
+        case "return":
+          const returnUl = document.getElementById(
+            `codecell-${currentCell}-return`
+          );
+
+          appendLi(returnUl, message.data);
+          currentCell = 0;
+          break;
+        case "end":
+          console.log(message.data);
+          break;
+      }
+    };
+  };
+
+  const handleCodeSubmit = event => {
+    clearPreviousResponse();
+
+    let cellNum = +event.target.dataset.button;
+    const codeStrArray = allCodeUpToCell(cellNum, codemirrors);
+    const json = JSON.stringify(codeStrArray);
+
+    ws.send(json);
+  };
+
+  const clearPreviousResponse = () => {
+    document.querySelectorAll(".code-return").forEach(codeReturn => {
+      removeChildElements(codeReturn);
+    });
+
+    document.querySelectorAll(".code-output").forEach(codeOutput => {
+      removeChildElements(codeOutput);
+    });
+
+    document.querySelectorAll(".code-error").forEach(codeError => {
+      removeChildElements(codeError);
+    });
+  };
+
+  document.addEventListener("click", event => {
+    if (event.target.className === "code-submit") {
+      handleCodeSubmit(event);
+    }
+  });
+
+  const removeChildElements = parent => {
+    while (parent.firstChild) {
+      parent.removeChild(parent.firstChild);
+    }
+  };
+
+  const appendLi = (parent, content) => {
+    const newLi = document.createElement("li");
+    newLi.textContent = content;
+    parent.appendChild(newLi);
+  };
+
+  // extract and render markdown
+  const mdCode = CodeMirror.fromTextArea(mdC1, {
+    mode: "markdown",
+    showCursorWhenSelecting: true,
+    theme: "monokai"
+  });
+
+  // load markdown-it parser into a variable
+  const md = window.markdownit();
+
+  btnMD1.addEventListener("click", () => {
+    let mdContent = mdCode.getValue();
+    let mdOutput = document.getElementById("md-1-result");
+    mdOutput.innerHTML = md.render(mdContent); // use markdown-it parser to render the markdown into html
+    mdCode.getWrapperElement().classList.add("hidden");
+  });
+});
+
+// maps all textareas designated as code cells to codemirror objects
+const setAllCodemirrorObjects = () => {
+  const codeCells = [...document.querySelectorAll(".code-cell")];
+
+  return codeCells.map(codeCell => {
+    let editor = CodeMirror.fromTextArea(codeCell, {
+      mode: "javascript",
+      theme: "darcula",
+      lineNumbers: true,
+      showCursorWhenSelecting: true
+    });
+
+    return editor;
+  });
+};
+
+// extracts and concatenates all code up to specified cell number
+const allCodeUpToCell = (cellNum, allCodeCells) => {
+  let codeStrArray = [];
+
+  for (let i = 0; i <= cellNum; i += 1) {
+    let cell = allCodeCells[i];
+    codeStrArray.push(
+      cell
+        .getValue()
+        .trim()
+        .concat("\n")
+    );
+  }
+
+  return codeStrArray;
+};
+
+// const appendCellOutput = resultObj => {
+//   Object.keys(resultObj)
+//     .slice(0, -2) // need to slice off return and result here
+//     .forEach(cellNumber => {
+//       const outputUl = document.getElementById(
+//         `codecell-${cellNumber}-output`
+//       );
+
+//       removeChildElements(outputUl);
+
+//       resultObj[cellNumber].stdout.forEach(output => {
+//         appendLi(outputUl, output);
+//       });
+//     });
+// };
+
+// const appendCellStderror = resultObj => {
+//   Object.keys(resultObj)
+//     .slice(0, -2) // need to slice off return and result here
+//     .forEach(cellNumber => {
+//       const errorUl = document.getElementById(`codecell-${cellNumber}-error`);
+//       const stderr = resultObj[cellNumber].stderr;
+//       removeChildElements(errorUl);
+
+//       if (stderr) {
+//         appendLi(errorUl, stderr);
+//       }
+//     });
+// };
+
+// const appendCellError = resultObj => {
+//   Object.keys(resultObj)
+//     .slice(0, -2) // need to slice off return and result here
+//     .forEach(cellNumber => {
+//       const errorUl = document.getElementById(`codecell-${cellNumber}-error`);
+
+//       if (resultObj[cellNumber].error) {
+//         const error = resultObj[cellNumber].error;
+
+//         if (error.signal && error.signal.match("SIGTERM")) {
+//           removeChildElements(errorUl);
+//           appendLi(errorUl, "Infinite Loop Error");
+//         } else if (error.code && String(error.code).match("MAXBUFFER")) {
+//           removeChildElements(errorUl);
+
+//           let stdout = resultObj[cellNumber].stdout;
+//           truncatedStdout = stdout.slice(0, 10);
+//           resultObj[cellNumber].stdout = truncatedStdout; // mutate resultObj to truncate stdout
+
+//           appendLi(errorUl, "Maximum Buffer Error");
+//         }
+//       }
+//     });
+// };
+
+// const appendCellReturn = (cellNum, resultObj) => {
+//   const codeReturn = document.getElementById(`codecell-${cellNum}-return`);
+//   const returnText = resultObj.return;
+
+//   document.querySelectorAll(".code-return").forEach(codeReturn => {
+//     removeChildElements(codeReturn);
+//   });
+
+//   if (returnText) {
+//     appendLi(codeReturn, returnText);
+//   }
+// };
+
+// mutates resultObj, converts stdout to an array with output mapped to correct cell
+// const mapStdoutToCell = resultObj => {
+//   let prevOutLength = 0;
+//   Object.keys(resultObj)
+//     .slice(0, -2) // need to slice off return and result here
+//     .forEach(cellNum => {
+//       const stdoutArr = resultObj[cellNum].stdout.split("\n").slice(0, -1);
+//       const newStdoutArr = stdoutArr.slice(prevOutLength);
+//       resultObj[cellNum].stdout = newStdoutArr;
+//       prevOutLength += stdoutArr.length;
+//     });
+//   return resultObj;
+// };
+
+// WRITE TO PAGE
+// listen for stdout / stderr / delimiters as they come across
+// slot appropriately & increment cellNum if DELIMITER
+// request.addEventListener("load", () => {
+//   const resultObj = request.response.responseObj;
+//   debugger;
+//   mapStdoutToCell(resultObj); // mutates each stdout in resultObj to an array
+//   appendCellStderror(resultObj);
+//   appendCellError(resultObj); // mutates stdout in resultObj for cell with MAXBUFFER error
+//   appendCellOutput(resultObj);
+//   appendCellReturn(cellNum, resultObj);
+// });
