f8ec99af431c58fb882f957d7365a4c4be6f462b
Charles Ging <charles.ging@gmail.com>
2019-11-12 17:20:22 -0600

    conditionally render a code cell, or markdown cell within CellsList

--- react-redpoint/src/Components/Cells/CellsList.jsx
+++ react-redpoint/src/Components/Cells/CellsList.jsx
@@ -1,23 +1,24 @@
 import React, { Component } from "react";
 import CodeCell from "./CodeCell";
+import ToggleableMarkdownContainer from "./ToggleableMarkdownContainer";
 
 class CellsList extends Component {
   state = {};
 
   render() {
     const codemirrorCells = this.props.cells.map((cell, index) => {
-      switch (cell.type) {
-        case "javascript":
-          return (
-            <CodeCell
-              key={index}
-              code={cell.code}
-              onAddCodeCellClick={this.props.onAddCodeCellClick}
-              cellIndex={index}
-            />
-          );
-        default:
-          throw new Error("Bad cell type provided");
+      if (cell.type === ("javascript" || "ruby" || "python")) {
+        return (
+          <CodeCell
+            language={cell.type}
+            key={index}
+            code={cell.code}
+            onAddCodeCellClick={this.props.onAddCodeCellClick}
+            cellIndex={index}
+          />
+        );
+      } else {
+        return <ToggleableMarkdownContainer />;
       }
     });
     return codemirrorCells;
