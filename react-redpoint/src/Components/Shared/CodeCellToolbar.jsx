01c6f81e29a3289fd6d678bfa52eda033572a3f2
William Mills <millswilliamrobert@gmail.com>
2019-11-13 11:33:38 -0500

    extracted codeCellToolbar into a reusable component

---
+++ react-redpoint/src/Components/Shared/CodeCellToolbar.jsx
@@ -0,0 +1,35 @@
+import React, { Component } from "react";
+import AddCodeCellButton from "../Shared/AddCodeCellButton";
+import DeleteCellButton from "./DeleteCellButton";
+import DropdownButton from "react-bootstrap/DropdownButton";
+import Dropdown from "react-bootstrap/Dropdown";
+
+const CodeCellToolbar = props => {
+  return (
+    <div className="code-cell-toolbar">
+      <DropdownButton
+        variant="secondary"
+        id="dropdown-basic-button"
+        title="Javascript"
+        size="sm"
+      >
+        <Dropdown.Item href="#/action-1">Markdown</Dropdown.Item>
+        <Dropdown.Item href="#/action-2" active>
+          Javascript
+        </Dropdown.Item>
+        <Dropdown.Item href="#/action-3">Ruby</Dropdown.Item>
+        <Dropdown.Item href="#/action-4">Python</Dropdown.Item>
+      </DropdownButton>
+      <AddCodeCellButton
+        onClick={props.onAddClick}
+        cellIndex={props.cellIndex}
+      />
+      <DeleteCellButton
+        onClick={props.onDeleteClick}
+        cellIndex={props.cellIndex}
+      />
+    </div>
+  );
+};
+
+export default CodeCellToolbar;
