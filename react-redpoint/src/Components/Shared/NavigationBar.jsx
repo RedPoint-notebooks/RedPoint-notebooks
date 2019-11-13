68fb1eb92a83ba3bdbb3721d1e410b06657485d1
William Mills <millswilliamrobert@gmail.com>
2019-11-13 17:21:03 -0500

    refactored handling of setting default language

--- react-redpoint/src/Components/Shared/NavigationBar.jsx
+++ react-redpoint/src/Components/Shared/NavigationBar.jsx
@@ -4,73 +4,59 @@ import NavDropdown from "react-bootstrap/NavDropdown";
 import Nav from "react-bootstrap/Nav";
 import logo from "../../placeholder_logo.svg";
 
-const NavigationBar = props => {
-  const setDefaultMarkdown = () => {
-    props.setDefaultLanguage("markdown");
-  };
-  const setDefaultJavascript = () => {
-    props.setDefaultLanguage("javascript");
-  };
-  const setDefaultRuby = () => {
-    props.setDefaultLanguage("ruby");
-  };
-  const setDefaultPython = () => {
-    props.setDefaultLanguage("python");
+class NavigationBar extends React.Component {
+  setDefaultLanguage = e => {
+    const language = e.target.value;
+    this.props.setDefaultLanguage(language);
   };
 
-  return (
-    <Navbar bg="dark" variant="dark">
-      <Navbar.Brand href="#home">
-        <img
-          alt=""
-          src={logo}
-          width="30"
-          height="30"
-          className="d-inline-block align-top"
-        />{" "}
-        RedPoint
-      </Navbar.Brand>
-      <Navbar.Toggle aria-controls="basic-navbar-nav" />
-      <Navbar.Collapse id="basic-navbar-nav">
-        <Nav className="mr-auto">
-          <Nav.Link href="#home">Share</Nav.Link>
-          <Nav.Link href="#foo">Clone</Nav.Link>
-          <Nav.Link href="#link">Delete</Nav.Link>
-          <NavDropdown title="Default Language" id="basic-nav-dropdown">
-            <NavDropdown.Item
-              as="button"
-              onClick={setDefaultMarkdown}
-              active={props.state.defaultLanguage === "markdown" ? true : false}
-            >
-              Markdown
-            </NavDropdown.Item>
-            <NavDropdown.Item
-              as="button"
-              onClick={setDefaultJavascript}
-              active={
-                props.state.defaultLanguage === "javascript" ? true : false
-              }
-            >
-              Javascript
-            </NavDropdown.Item>
-            <NavDropdown.Item
-              as="button"
-              onClick={setDefaultRuby}
-              active={props.state.defaultLanguage === "ruby" ? true : false}
-            >
-              Ruby
-            </NavDropdown.Item>
-            <NavDropdown.Item
-              as="button"
-              onClick={setDefaultPython}
-              active={props.state.defaultLanguage === "python" ? true : false}
-            >
-              Python
-            </NavDropdown.Item>
-          </NavDropdown>
-        </Nav>
-      </Navbar.Collapse>
-    </Navbar>
-  );
-};
+  render() {
+    const capitalized = string => {
+      return string.charAt(0).toUpperCase() + string.slice(1);
+    };
+    // TODO: pull language options from a config file
+    const navDropDownItems = ["markdown", "javascript", "ruby", "python"].map(
+      language => {
+        return (
+          <NavDropdown.Item
+            as="button"
+            value={language}
+            onClick={this.setDefaultLanguage}
+            active={
+              this.props.state.defaultLanguage === { language } ? true : false
+            }
+          >
+            {capitalized(language)}
+          </NavDropdown.Item>
+        );
+      }
+    );
+
+    return (
+      <Navbar bg="dark" variant="dark">
+        <Navbar.Brand href="#home">
+          <img
+            alt=""
+            src={logo}
+            width="30"
+            height="30"
+            className="d-inline-block align-top"
+          />{" "}
+          RedPoint
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="mr-auto">
+            <Nav.Link href="#home">Share</Nav.Link>
+            <Nav.Link href="#foo">Clone</Nav.Link>
+            <Nav.Link href="#link">Delete</Nav.Link>
+            <NavDropdown title="Default Language" id="basic-nav-dropdown">
+              {navDropDownItems}
+            </NavDropdown>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
+    );
+  }
+}
 export default NavigationBar;
