51140814748c10258903e34c70e5a68546ef4682
Charles Ging <charles.ging@gmail.com>
2019-11-15 11:54:30 -0600

    convert codeCellContainer to a SFC

--- react-redpoint/src/Components/Cells/CellsList.jsx
+++ react-redpoint/src/Components/Cells/CellsList.jsx
@@ -8,15 +8,11 @@ const CellsList = props => {
     return (
       <CodeCellContainer
         cell={cell}
-        // language={cell.type}
         key={uuidv4()}
-        // code={cell.code}
-        // results={cell.results}
         onDeleteCellClick={props.onDeleteCellClick}
         onAddCellClick={props.onAddCellClick}
         cellIndex={index}
         defaultLanguage={props.defaultLanguage}
-        // rendered={cell.rendered}
         onLanguageChange={props.onLanguageChange}
         toggleRender={props.toggleRender}
         onUpdateCodeState={props.onUpdateCodeState}
@@ -39,41 +35,3 @@ const CellsList = props => {
 };
 
 export default CellsList;
-
-// class CellsList extends Component {
-//   render() {
-//     const cellContainers = this.props.cells.map((cell, index) => {
-//       return (
-//         <CodeCellContainer
-//           cell={cell}
-//           // language={cell.type}
-//           key={uuidv4()}
-//           // code={cell.code}
-//           // results={cell.results}
-//           onDeleteCellClick={this.props.onDeleteCellClick}
-//           onAddCellClick={this.props.onAddCellClick}
-//           cellIndex={index}
-//           defaultLanguage={this.props.defaultLanguage}
-//           // rendered={cell.rendered}
-//           onLanguageChange={this.props.onLanguageChange}
-//           toggleRender={this.props.toggleRender}
-//           onUpdateCodeState={this.props.onUpdateCodeState}
-//           onRunClick={this.props.onRunClick}
-//         />
-//       );
-//     });
-
-//     cellContainers.push(
-//       <AddCellButton
-//         soloButton="true"
-//         cellIndex={cellContainers.length}
-//         onClick={this.props.onAddCellClick}
-//         key={uuidv4()}
-//         defaultLanguage={this.props.defaultLanguage}
-//       />
-//     );
-//     return cellContainers;
-//   }
-// }
-
-// export default CellsList;
--- react-redpoint/src/Components/Cells/CodeCellContainer.jsx
+++ react-redpoint/src/Components/Cells/CodeCellContainer.jsx
@@ -2,40 +2,77 @@ import React, { Component } from "react";
 import CodeCell from "./CodeCell";
 import RenderedMarkdown from "./RenderedMarkdown";
 
-class CodeCellContainer extends Component {
-  render() {
-    const cell = this.props.cell;
-    const isRenderedMarkdown = cell.type === "Markdown" && cell.rendered;
-    return isRenderedMarkdown ? (
-      <RenderedMarkdown
-        language={cell.type}
-        code={cell.code}
-        cellIndex={this.props.cellIndex}
-        onDeleteClick={this.props.onDeleteCellClick}
-        onAddClick={this.props.onAddCellClick}
-        defaultLanguage={this.props.defaultLanguage}
-        onRenderedMarkdownClick={this.props.toggleRender}
-        onLanguageChange={this.props.onLanguageChange}
-        rendered={cell.rendered}
-      />
-    ) : (
-      <CodeCell
-        language={cell.type}
-        key={this.props.index}
-        code={cell.code}
-        results={cell.results}
-        onAddClick={this.props.onAddCellClick}
-        onDeleteClick={this.props.onDeleteCellClick}
-        cellIndex={this.props.cellIndex}
-        defaultLanguage={this.props.defaultLanguage}
-        onLanguageChange={this.props.onLanguageChange}
-        toggleRender={this.props.toggleRender}
-        onUpdateCodeState={this.props.onUpdateCodeState}
-        rendered={cell.rendered}
-        onRunClick={this.props.onRunClick}
-      />
-    );
-  }
-}
+const CodeCellContainer = props => {
+  const cell = props.cell;
+  const isRenderedMarkdown = cell.type === "Markdown" && cell.rendered;
+
+  return isRenderedMarkdown ? (
+    <RenderedMarkdown
+      language={cell.type}
+      code={cell.code}
+      cellIndex={props.cellIndex}
+      onDeleteClick={props.onDeleteCellClick}
+      onAddClick={props.onAddCellClick}
+      defaultLanguage={props.defaultLanguage}
+      onRenderedMarkdownClick={props.toggleRender}
+      onLanguageChange={props.onLanguageChange}
+      rendered={cell.rendered}
+    />
+  ) : (
+    <CodeCell
+      language={cell.type}
+      key={props.index}
+      code={cell.code}
+      results={cell.results}
+      onAddClick={props.onAddCellClick}
+      onDeleteClick={props.onDeleteCellClick}
+      cellIndex={props.cellIndex}
+      defaultLanguage={props.defaultLanguage}
+      onLanguageChange={props.onLanguageChange}
+      toggleRender={props.toggleRender}
+      onUpdateCodeState={props.onUpdateCodeState}
+      rendered={cell.rendered}
+      onRunClick={props.onRunClick}
+    />
+  );
+};
 
 export default CodeCellContainer;
+
+// class CodeCellContainer extends Component {
+//   render() {
+//     const cell = this.props.cell;
+//     const isRenderedMarkdown = cell.type === "Markdown" && cell.rendered;
+//     return isRenderedMarkdown ? (
+//       <RenderedMarkdown
+//         language={cell.type}
+//         code={cell.code}
+//         cellIndex={this.props.cellIndex}
+//         onDeleteClick={this.props.onDeleteCellClick}
+//         onAddClick={this.props.onAddCellClick}
+//         defaultLanguage={this.props.defaultLanguage}
+//         onRenderedMarkdownClick={this.props.toggleRender}
+//         onLanguageChange={this.props.onLanguageChange}
+//         rendered={cell.rendered}
+//       />
+//     ) : (
+//       <CodeCell
+//         language={cell.type}
+//         key={this.props.index}
+//         code={cell.code}
+//         results={cell.results}
+//         onAddClick={this.props.onAddCellClick}
+//         onDeleteClick={this.props.onDeleteCellClick}
+//         cellIndex={this.props.cellIndex}
+//         defaultLanguage={this.props.defaultLanguage}
+//         onLanguageChange={this.props.onLanguageChange}
+//         toggleRender={this.props.toggleRender}
+//         onUpdateCodeState={this.props.onUpdateCodeState}
+//         rendered={cell.rendered}
+//         onRunClick={this.props.onRunClick}
+//       />
+//     );
+//   }
+// }
+
+// export default CodeCellContainer;
