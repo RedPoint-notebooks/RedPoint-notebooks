ca88ee393d0e224140be09e397573b037fefe537
Charles Ging <charles.ging@gmail.com>
2019-11-15 12:07:28 -0600

    refactored CodeCell to take a cell prop

--- react-redpoint/src/Components/Cells/CodeCellContainer.jsx
+++ react-redpoint/src/Components/Cells/CodeCellContainer.jsx
@@ -18,7 +18,7 @@ const CodeCellContainer = props => {
     />
   ) : (
     <CodeCell
-      // cell={cell}
+      cell={cell}
       language={cell.type}
       key={props.index}
       code={cell.code}
@@ -37,41 +37,3 @@ const CodeCellContainer = props => {
 };
 
 export default CodeCellContainer;
-
-// class CodeCellContainer extends Component {
-//   render() {
-//     const cell = this.props.cell;
-//     const isRenderedMarkdown = cell.type === "Markdown" && cell.rendered;
-//     return isRenderedMarkdown ? (
-//       <RenderedMarkdown
-//         language={cell.type}
-//         code={cell.code}
-//         cellIndex={this.props.cellIndex}
-//         onDeleteClick={this.props.onDeleteCellClick}
-//         onAddClick={this.props.onAddCellClick}
-//         defaultLanguage={this.props.defaultLanguage}
-//         onRenderedMarkdownClick={this.props.toggleRender}
-//         onLanguageChange={this.props.onLanguageChange}
-//         rendered={cell.rendered}
-//       />
-//     ) : (
-//       <CodeCell
-//         language={cell.type}
-//         key={this.props.index}
-//         code={cell.code}
-//         results={cell.results}
-//         onAddClick={this.props.onAddCellClick}
-//         onDeleteClick={this.props.onDeleteCellClick}
-//         cellIndex={this.props.cellIndex}
-//         defaultLanguage={this.props.defaultLanguage}
-//         onLanguageChange={this.props.onLanguageChange}
-//         toggleRender={this.props.toggleRender}
-//         onUpdateCodeState={this.props.onUpdateCodeState}
-//         rendered={cell.rendered}
-//         onRunClick={this.props.onRunClick}
-//       />
-//     );
-//   }
-// }
-
-// export default CodeCellContainer;
