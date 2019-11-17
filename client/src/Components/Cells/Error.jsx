f693171c3013a548ae3e3bd39b3090981a09f85c
ridergit <benjaminriderharvey@gmail.com>
2019-11-16 21:21:34 -0500

    Adds helper to add line breaks to error messages

--- client/src/Components/Cells/Error.jsx
+++ client/src/Components/Cells/Error.jsx
@@ -2,9 +2,19 @@ import React from "react";
 import ListGroup from "react-bootstrap/ListGroup";
 
 const Error = props => {
+  // const brError = props.error.replace(/\n/g, "\r\n");
+
+  const addLineBreaks = string =>
+    string.split("\n").map((text, index) => (
+      <React.Fragment key={`${text}-${index}`}>
+        {text}
+        <br />
+      </React.Fragment>
+    ));
+
   return props.error ? (
     <ListGroup.Item className="output" variant="danger">
-      {props.error}
+      {addLineBreaks(props.error)}
     </ListGroup.Item>
   ) : null;
 };
