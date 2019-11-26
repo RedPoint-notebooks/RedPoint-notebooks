25f2bd48b7cd0f99872ed4fc70a12e45ea3cbf33
Charles G <charles.ging@gmail.com>
2019-11-26 14:47:25 -0600

    Revert "Save notebook production (#77)" (#78)
    This reverts commit 76a96a4b3922d09880fe38732e93f00871525b05.

--- client/src/Components/Notebook.jsx
+++ client/src/Components/Notebook.jsx
@@ -27,7 +27,7 @@ class Notebook extends Component {
   ws = null;
 
   loadState = async () => {
-    await fetch(`/loadNotebook`, {
+    await fetch(`${window.location.host}/loadNotebook`, {
       method: "GET",
       mode: "cors",
       cache: "no-cache",
