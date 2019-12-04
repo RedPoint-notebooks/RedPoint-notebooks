97013349fe4bf7e96191f7a647c95c09174e5620
ridergit <benjaminriderharvey@gmail.com>
2019-12-04 13:26:41 -0500

    Fixes bug in constants.js where split array of domain wasn't joined on periods

--- client/src/Constants/constants.js
+++ client/src/Constants/constants.js
@@ -2,5 +2,8 @@ export const LANGUAGES = ["Markdown", "Javascript", "Ruby", "Python"];
 export const SIGTERM_ERROR_MESSAGE = `Timeout Error: the request to the server timed out.
               The maximum timeout threshold for server requests is currently 5 seconds.
               Check your code for infinite loops, or timeouts greater than the threshold.`;
-const domain = window.location.host.split(".").splice(-2);
+const domain = window.location.host
+  .split(".")
+  .splice(-2)
+  .join(".");
 export const PROXY_URL = "http://www." + domain;
